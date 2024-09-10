45d33f7e4c401b2d4d19584081d59afe5626ad6d
Santos Alberto Ortiz Chavez <sortiz@internal.inss.org>
2024-09-10 13:12:31 -0600

    Add new print tasks

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/santos95/GoBasics/Variables"
 )
 
 func main() {
@@ -9,4 +10,6 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("My name is Santos, I am learning Go ... !!!")
 
+	fmt.Println("Print a value using a imported method: ")
+	Variables.ShowIntegers()
 }
